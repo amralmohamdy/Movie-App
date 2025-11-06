23b987f5e11ee231011e8b79f243d39c4ef3be89
sayed <sayedshabaan204@gmail.com>
2025-11-06 21:18:33 +0200

    update commit message

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Watchlist } from './components/watchlist/watchlist';
+import { Homepage } from './components/home/homepage/homepage';
+import { Login } from './components/login/login';
+import { Footer } from './components/footer/footer';
+import { Details } from './components/details/details';
+import { Register } from './components/register/register';
+import { NotFoundError } from 'rxjs';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'',component:Homepage},
+    {path:'watchlist',component:Watchlist},
+    {path:'footer',component:Footer},
+    {path:'details',component:Details},
+    {path:'login',component:Login},
+    {path:'register',component:Register},
+    {path:'**',component:NotFoundError},
+];
