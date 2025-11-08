f0c094af0f20587138a09efe7080a1242046e3d8
amralmohamdy <amralmohamdy@gmail.com>
2025-11-08 22:31:46 +0200

    add dark/ligh mode toggle button to header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,14 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 import { MoviesResources } from '../../shared/movies-resources';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink,RouterLink,RouterLinkActive],
+  imports: [RouterLink, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
+export class Header implements OnInit {
   // controls whether the mobile menu is visible (small screens)
   menuOpen: boolean = false;
 
@@ -23,6 +24,32 @@ export class Header {
   svc = inject(MoviesResources);
   changeLanguage(lang: string) {
     this.svc.lang.set(lang);
+    const dir = lang.startsWith('ar') ? 'rtl' : 'ltr';
+    document.documentElement.setAttribute('dir', dir);
+    console.log('Language Changed:', lang, 'Direction:', dir);
+  }
+
+  isDarkMode = false;
+
+  ngOnInit() {
+    // لو المستخدم اختار مود قبل كده، نحفظه
+    const theme = localStorage.getItem('theme') || 'light';
+    this.applyTheme(theme);
+    //event on signal
+    // Reactively update dir when lang changes
+
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    const newTheme = this.isDarkMode ? 'dark' : 'light';
+    this.applyTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  }
+
+  applyTheme(theme: string) {
+    document.documentElement.setAttribute('data-theme', theme);
+    this.isDarkMode = theme === 'dark';
   }
 
 }
